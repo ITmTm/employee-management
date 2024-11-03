3b93c855722651ed67e9d47d08a22944c1f4af0c
Timur <Stafr95@mail.ru>
2024-11-03 21:18:48 +0500

    Component creation

---
+++ src/components/employeeSort/EmployeeSort.jsx
@@ -0,0 +1,35 @@
+import {useDispatch, useSelector} from "react-redux";
+import { setSort } from "../../redux/employeeSlice.js";
+
+import './employeeSort.scss';
+
+const EmployeeSort = () => {
+	const dispatch = useDispatch();
+	const sort = useSelector((state) => state.employees.sort);
+
+	const handleSortChange = (e) => {
+		dispatch(setSort({ ...sort, by: e.target.value }));
+	};
+
+	const toggleSortDirection = () => {
+		const newDirection = sort.direction === 'asc' ? 'desc' : 'asc';
+		dispatch(setSort({ ...sort, direction: newDirection }));
+	};
+
+	return (
+		<div className='employee-sort'>
+			<label>
+				Сортировать по:
+				<select value={sort.by} onChange={handleSortChange}>
+					<option value="name">Имя</option>
+					<option value="birthday">Дата рождения</option>
+				</select>
+			</label>
+			<button onClick={toggleSortDirection}>
+				{sort.direction === 'asc' ? 'По возрастанию' : 'по убыванию'}
+			</button>
+		</div>
+	);
+};
+
+export default EmployeeSort;
