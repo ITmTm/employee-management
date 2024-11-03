ca358bc4ea7e1d91dc73cd01026b6dbcd4d482cb
Timur <Stafr95@mail.ru>
2024-11-03 21:21:24 +0500

    Component creation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import {StrictMode} from "react";
+import { createRoot } from 'react-dom/client';
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from './components/app/App.jsx'
+import store from './redux/store.js'
+
+import './styles/main.scss'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
 )
