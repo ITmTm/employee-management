5c929cacab4923dde69c68d822c8559656e9fab5
Timur <Stafr95@mail.ru>
2024-11-04 20:45:22 +0500

    Component creation

--- src/pages/employeeEditPage/EmployeeEditPage.jsx
+++ src/pages/employeeEditPage/EmployeeEditPage.jsx
@@ -1,15 +1,22 @@
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 import EmployeeAddForm from "../../components/employeeAddForm/EmployeeAddForm.jsx";
 
 import './employeeEditPage.scss';
 
 const EmployeeEditPage = () => {
     const { id } = useParams();
+    const employee = useSelector(state => state.employees.list.find(emp => emp.id === Number(id)));
+
 
     return (
         <div className='employee-edit-page'>
             <h1>Редактирование сотрудника</h1>
-            <EmployeeAddForm employeeId={true} />
+          {employee ? (
+            <EmployeeAddForm isEditMode={true} />
+          ) : (
+            <p>Сотрудник не найден</p>
+          )}
         </div>
     );
 };
