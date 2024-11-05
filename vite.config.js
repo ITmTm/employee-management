d28cb7be0b67985cf3ebdf78a62d7a3638a0c465
Timur <Stafr95@mail.ru>
2024-11-05 13:02:51 +0500

    Change config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin()
+  ],
   css: {
     preprocessorOptions: {
       scss: {
         silenceDeprecations: ["legacy-js-api"],
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/__tests__/jest.setup.js'],
   },
 })
